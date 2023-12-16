41d054b0da466037cf5c1d895792e1cb0c7279d8
VitaliiSenkiv <vitalii.senkiv15@gmail.com>
2023-12-16 20:59:44 +0200

    Fitted AnalysisTypeDTO for front-end

--- Domain/DTO/AnalysisTypeDTO.cs
+++ Domain/DTO/AnalysisTypeDTO.cs
@@ -1,11 +1,14 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace MedApp.Domain.DTO
 {
     public class AnalysisTypeDTO
     {
+        [JsonProperty("name")]
         public string Name { get; set; } = string.Empty;
 
+        [JsonProperty("fields")]
         public JArray FieldsData { get; set; }
     }
 }
