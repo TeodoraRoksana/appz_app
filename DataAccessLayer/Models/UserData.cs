3b724377c5faa6c0d7a897d0179eab48010cc147
VitaliiSenkiv <vitalii.senkiv15@gmail.com>
2023-12-16 16:29:36 +0200

    Renamed AnalysisDataDTO file

--- DataAccessLayer/Models/UserData.cs
+++ DataAccessLayer/Models/UserData.cs
@@ -1,10 +1,5 @@
 ﻿using Domain.Models.Base;
 using MedApp.DataAccessLayer.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Domain.Models
 {
