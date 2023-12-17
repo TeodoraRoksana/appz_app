b91c02df48fbb95d31905d9c6ae5fdbc1982f895
VitaliiSenkiv <vitalii.senkiv15@gmail.com>
2023-12-17 13:07:36 +0200

    Updated json property name for AnalysisShortDataDTO to match front-end

--- Domain/DTO/AnalysisShortDataDTO.cs
+++ Domain/DTO/AnalysisShortDataDTO.cs
@@ -1,15 +1,22 @@
-﻿namespace MedApp.Domain.DTO
+﻿using Newtonsoft.Json;
+
+namespace MedApp.Domain.DTO
 {
     public class AnalysisShortDataDTO
     {
+        [JsonProperty("id")]
         public int AnalysisId { get; set; } = 0;
 
+        [JsonProperty("type")]
         public string AnalysisName { get; set; } = string.Empty;
 
+        [JsonProperty("patient_name")]
         public string PatientName { get; set; } = string.Empty;
 
+        [JsonProperty("patient_surname")]
         public string PatientSurname { get; set; } = string.Empty;
 
+        [JsonProperty("date")]
         public string? Date { get; set; }
     }
 }
