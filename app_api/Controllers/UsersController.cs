e87bf8d73253b38bfbad4a9494115cabf8d09f11
NazariiSenkiv <95177352+NazariiSenkiv@users.noreply.github.com>
2023-12-17 11:51:09 +0200

    Added Users endpoint

---
+++ app_api/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Models;
+using MedApp.Domain.DTO;
+using MedApp.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MedApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : Controller
+    {
+        private IUserService _service;
+
+        public UsersController(IUserService service)
+        {
+            _service = service;
+        }
+
+        // GET api/<UsersController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserDataDTO>> Get(int id)
+        {
+            try
+            {
+                var analysis = await _service.GetUserByIdAsync(id);
+
+                return Ok(analysis);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
